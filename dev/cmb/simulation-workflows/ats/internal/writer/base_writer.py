523970319786dcd4b90a993169ada93ed6c46506
John Tourtellott <john.tourtellott@kitware.com>
2020-05-08 14:41:32 -0400

    Update writers again, mostly for resource and surface mesh types

--- dev/cmb/simulation-workflows/ats/internal/writer/base_writer.py
+++ dev/cmb/simulation-workflows/ats/internal/writer/base_writer.py
@@ -18,6 +18,7 @@ from xml.dom import minidom
 
 import smtk
 import smtk.attribute
+import smtk.model
 
 from .shared_data import instance as shared
 
@@ -41,6 +42,24 @@ class BaseWriter:
         # Do NOT include any member data
         pass
 
+    def _get_native_model_path(self, resource):
+        """"""
+        # We only support model resource filenames for now
+        model_res = smtk.model.Resource.CastTo(resource)
+        if model_res is None:
+            return None
+
+        # Unfortunately, this logic is all part of SMTK lore.
+        uuids = model_res.entitiesMatchingFlags(smtk.model.MODEL_ENTITY, True)
+        if not uuids:
+            raise RuntimeError('No model entities in model resource')
+
+        model_uuid = uuids.pop()
+        if not model_res.hasStringProperty(model_uuid, 'url'):
+            raise RuntimeError('Model resource missing \"url\" property')
+        prop_list = model_res.stringProperty(model_uuid, 'url')
+        return prop_list[0]
+
     def _new_list(self, parent, list_name, list_type='ParameterList'):
         """Appends ParameterList element to parent"""
         new_list = shared.xml_doc.createElement('ParameterList')
@@ -57,6 +76,45 @@ class BaseWriter:
         new_param.setAttribute('value', param_value)
         list_elem.appendChild(new_param)
         return new_param
+
+    def _render_associations(self, parent_elem, att, elem_name_or_list):
+        """Generates Parameter element for attribute associations."""
+        ref_item = att.associations()
+        if ref_item is None:
+            print('Warning: expected attribute \"{}\" to have associations'.format(att.name()))
+            return
+
+        n = ref_item.numberOfValues()
+        if n == 0:
+            print('Warning: expected attribute \"{}\" to have associations'.format(att.name()))
+            return
+
+        if isinstance(elem_name_or_list, (list, tuple)) and len(elem_name_or_list) > 1:
+            elem_name = elem_name_or_list[0]
+            array_name = elem_name_or_list[1]
+        else:
+            elem_name = elem_name_or_list
+            array_name = elem_name
+
+        # Generate list of attribute names
+        value_list = list()
+        for i in range(n):
+            if ref_item.isSet(i):
+                value_att = ref_item.value(i)
+                value_list.append(value_att.name())
+
+        if len(value_list) == 0:
+            print('Warning: expected attribute \"{}\" to have associations'.format(att.name()))
+            return
+
+        if len(value_list) == 1:
+            self._new_param(parent_elem, elem_name, 'string', value_list[0])
+            return
+
+        # (else) n > 1
+        value_string = ', '.join(value_list)
+        array_string = '{{{}}}'.format(value_string)
+        self._new_param(parent_elem, array_name, 'Array(string)', array_string)
 
     def _render_items(self, parent_elem, att, param_names):
         """Generates Parameter elements for items specified by param_names"""
--- dev/cmb/simulation-workflows/ats/internal/writer/domain_writer.py
+++ dev/cmb/simulation-workflows/ats/internal/writer/domain_writer.py
@@ -30,44 +30,74 @@ class DomainWriter(BaseWriter):
 
     def write(self, xml_root):
         """"""
-        # lookup table for mesh type
-        mesh_type_dict = {
-            'mesh.generate-mesh': 'generate mesh',
+        # Lookup table for mesh type to xml attribute
+        mesh_type_params = {
+            'mesh.generate': 'generate mesh',
             'mesh.resource': 'read mesh file',
             'mesh.surface': 'surface',
         }
 
-        # possible children parameters
-        children = {
+        # Association parameters
+        assoc_params = {
+            'mesh.surface': ['surface sideset name', 'surface sideset names'],
+        }
+
+        # possible children_params parameters
+        children_params = {
             'mesh.generate': ['domain low coordinate', 'domain high coordinate', 'number of cells'],
-            'mesh.resource': ['file', 'format'],
-            'mesh.surface': ['surface sideset name', 'export mesh to file', ], # TODO: more
+            'mesh.resource': [],
+            'mesh.surface': ['export mesh to file', ],
             'subgrid': ['subgrid region name', 'entity kind', 'parent domain', 'flyweight mesh'],
             # TODO: column mesh
         }
         main_param_names = ['verify mesh', 'deformable mesh']
-        # Note about `'partitioner'`: it only makes sense on the "domain" mesh
+
         ####
         # Logic to render the mesh section
         mesh_elem = self._new_list(xml_root, 'mesh')
         domain_atts = shared.sim_atts.findAttributes('mesh')
         for domain_att in domain_atts:
-            # save out each domain
-            domain_elem = self._new_list(mesh_elem, domain_att.name())
-            mesh_type = mesh_type_dict.get(domain_att.type())
-            if mesh_type is None:
+            # Write out each domain (mesh)
+            mesh_type = domain_att.type()
+            mesh_type_string = mesh_type_params.get(domain_att.type())
+            if mesh_type_string is None:
                 raise NotImplementedError('Unsupported mesh type', domain_att.type())
-            _ = self._new_param(domain_elem, 'mesh type', 'string', mesh_type)
-            #  Winging it here to generate the parameters list
-            gen_params_list_name = '{} parameters'.format(mesh_type)
+
+            # Create list element
+            domain_elem = self._new_list(mesh_elem, domain_att.name())
+            _ = self._new_param(domain_elem, 'mesh type', 'string', mesh_type_string)
+
+            # Special cases
+            if mesh_type == 'mesh.resource':
+                resource_item = domain_att.find('resource')
+                resource = resource_item.value()
+                if resource is None:
+                    raise RuntimeError('Model not loaded for ResourceItem {}'.format(item.name()))
+                path = self._get_native_model_path(resource)
+                if path is None:
+                    raise RuntimeError('Model file not found for ResourceItem {}'.format(item.name()))
+                # For now, just write the base filename
+                filename = os.path.basename(path)
+                self._new_param(domain_elem, 'file', 'string', filename)
+                # Todo Extend to include MSTK mesh files
+                self._new_param(domain_elem, 'format', 'string', 'Exodus II')
+
+            #  Children params
+            gen_params_list_name = '{} parameters'.format(mesh_type_string)
             gen_params_list = self._new_list(domain_elem, gen_params_list_name)
-            known_children = children.get(mesh_type, []) + main_param_names
-            self._render_items(gen_params_list, domain_att, known_children)
+
+            # Association params
+            assoc_params_list = assoc_params.get(mesh_type)
+            if assoc_params_list:
+                self._render_associations(gen_params_list, domain_att, assoc_params_list)
+
+            # Remaining params
+            known_children_params = children_params.get(mesh_type, [])
+            self._render_items(gen_params_list, domain_att, known_children_params)
             # TODO: If a `domain` mesh, not a surface or otherwise, add the partitioner option:
             #       there aren't any examples of this being used, so leaving out
             # if domain mesh: # psuedo-code
             #     self._render_items(gen_params_list, type_item, ['partitioner',])
             # Top level mesh parameters
-
-            # self._render_items(domain_elem, domain_att, main_param_names)
+            self._render_items(domain_elem, domain_att, main_param_names)
         return
