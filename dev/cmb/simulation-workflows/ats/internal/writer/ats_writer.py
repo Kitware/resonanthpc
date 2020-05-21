99d1c047502db1062d7dc21c47f3702bf1fbb433
banesullivan <banesulli@gmail.com>
2020-05-21 15:21:25 -0400

    Get writer in working order for demo 01

--- dev/cmb/simulation-workflows/ats/internal/writer/ats_writer.py
+++ dev/cmb/simulation-workflows/ats/internal/writer/ats_writer.py
@@ -101,9 +101,24 @@ class ATSWriter(BaseWriter):
         imp.reload(region_writer)
         region_writer.RegionWriter().write(self.xml_root)
 
-        ################################
+        from . import coordinator_writer
+        imp.reload(coordinator_writer)
+        coordinator_writer.CoordinatorWriter().write(self.xml_root)
 
-        ## TODO: implement other writers
+        from . import checkpoint_writer
+        imp.reload(checkpoint_writer)
+        checkpoint_writer.CheckpointWriter().write(self.xml_root)
 
-        ################################
+        from . import pk_writer
+        imp.reload(pk_writer)
+        pk_writer.PKWriter().write(self.xml_root)
+
+        from . import state_writer
+        imp.reload(state_writer)
+        state_writer.StateWriter().write(self.xml_root)
+
+        from . import vis_writer
+        imp.reload(vis_writer)
+        vis_writer.VisualizationWriter().write(self.xml_root)
+
         return shared.xml_doc
