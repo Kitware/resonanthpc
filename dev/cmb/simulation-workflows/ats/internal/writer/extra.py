f28db7d9f73eb0b151aad8512190177179be80f7
banesullivan <banesulli@gmail.com>
2020-05-21 10:06:11 -0400

    Add PK_MAPPING and finish coordinator writer

---
+++ dev/cmb/simulation-workflows/ats/internal/writer/extra.py
@@ -0,0 +1,12 @@
+"""Some extra stuff."""
+
+PK_MAPPING = {
+    "pk-richards": "richards steady state",
+    # "": "",
+    # "": "",
+    # "": "",
+
+}
+"""This dict is a mapping bewteen the AttDef names we have and the actual names
+of the different Process Kernels in ATS.
+"""
