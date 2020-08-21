e548fbce1ff806f4241685dbf5154f6140c4d9fe
banesullivan <bane.sullivan@kitware.com>
2020-08-21 07:49:14 -0600

    mkdir for demos

--- dev/cmb/demo/make_resources.sh
+++ dev/cmb/demo/make_resources.sh
@@ -6,24 +6,28 @@ TESTS="../simulation-workflows/ats/internal/tests/"
 EXPORTER="../simulation-workflows/export_ats.py"
 
 # Demos 01-04
+mkdir -p demo-01
 pvpython $BUILDER \
   $TEMPLATE \
   "${TESTS}/test_demos/demo.01.yml" \
   -o demo-01/demo.01.smtk
 pvpython $EXPORTER demo-01/demo.01.smtk -o demo-01/demo.01.xml
 
+mkdir -p demo-02
 pvpython $BUILDER \
   $TEMPLATE \
   "${TESTS}/test_demos/demo.02.yml" \
   -o demo-02/demo.02.smtk
 pvpython $EXPORTER demo-02/demo.02.smtk -o demo-02/demo.02.xml
 
+mkdir -p demo-03
 pvpython $BUILDER \
   $TEMPLATE \
   "${TESTS}/test_demos/demo.03.yml" \
   -o demo-03/demo.03.smtk
 pvpython $EXPORTER demo-03/demo.03.smtk -o demo-03/demo.03.xml
 
+mkdir -p demo-04
 pvpython $BUILDER \
   $TEMPLATE \
   "${TESTS}/test_demos/demo.04.yml" \
