be106f5dcbfa092d24ed197f412904089f672b43
banesullivan <bane.sullivan@kitware.com>
2020-09-11 11:46:48 -0600

    Make run_ats.sh linux compatible

--- dev/cmb/demo/run-ats.sh
+++ dev/cmb/demo/run-ats.sh
@@ -2,7 +2,7 @@
 # Accepts name of directory with simulation input deck
 set -e
 # Outputs will be saved there as well in `sim_dump`
-pushd $1
+cd $1
 rm -rf sim_dump
 original_files=$(ls | grep -Ev '\.(smtk|ipynb)$')
 mkdir sim_dump
@@ -17,9 +17,9 @@ CONT_MNT=/home/amanzi_usr/work
 CONT_PWD=/home/amanzi_usr/work
 
 # WARNING: If there are more than one, this won't work
-input_file=($(find . -type f -name "*.xml"))
+input_file=$(find . -type f -name "*.xml")
 
 docker run --rm -v $HOST_MNT:$CONT_MNT:delegated -w $CONT_PWD metsi/ats mpirun -n 4 ats --xml_file=$input_file
 
 # Return
-popd
+cd ../../
