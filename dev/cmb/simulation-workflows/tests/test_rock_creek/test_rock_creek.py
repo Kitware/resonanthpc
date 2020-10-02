b915087cbe2d5f5b3c9cff6aeab8a88ab817089c
banesullivan <bane.sullivan@kitware.com>
2020-10-02 12:10:27 -0600

    Add spinup-homo rock creek example

---
+++ dev/cmb/simulation-workflows/tests/test_rock_creek/test_rock_creek.py
@@ -0,0 +1,21 @@
+import os
+import unittest
+
+from tests.base import BaseTestCase
+
+
+class SpinupHomoTest(BaseTestCase):
+
+    MODEL_RESOURCE_FILENAME = "data/mesh.rock_creek.smtk"
+    BASELINE_XML_FILENAME = "spinup-homo.xml"
+    SOURCE_DIR = os.path.abspath(os.path.dirname(__file__))
+    YAML_RESOURCE = "spinup-homo.yml"
+
+    def test(self):
+        self.writer.generate_xml()
+        xml_string = self.writer.get_xml_doc(pretty=True)
+        self._compare_xml_content(xml_string)
+
+
+if __name__ == "__main__":
+    unittest.main()
