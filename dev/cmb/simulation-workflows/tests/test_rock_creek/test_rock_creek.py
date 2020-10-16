55809257007067f6a5a45bdf7c2a642b5f362a04
banesullivan <bane.sullivan@kitware.com>
2020-10-16 15:38:42 -0600

    Initial Rock Creek template demo

--- dev/cmb/simulation-workflows/tests/test_rock_creek/test_rock_creek.py
+++ dev/cmb/simulation-workflows/tests/test_rock_creek/test_rock_creek.py
@@ -17,5 +17,18 @@ class SpinupHomoTest(BaseTestCase):
         self._compare_xml_content(xml_string)
 
 
+class TemplateTest(BaseTestCase):
+
+    MODEL_RESOURCE_FILENAME = "data/mesh.rock_creek.smtk"
+    BASELINE_XML_FILENAME = "template.xml"
+    SOURCE_DIR = os.path.abspath(os.path.dirname(__file__))
+    YAML_RESOURCE = "template.yml"
+
+    def test(self):
+        self.writer.generate_xml()
+        xml_string = self.writer.get_xml_doc(pretty=True)
+        self._compare_xml_content(xml_string)
+
+
 if __name__ == "__main__":
     unittest.main()
