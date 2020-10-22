e7295acbedab5c2e9e1eb375d56bdff875318449
John Tourtellott <john.tourtellott@kitware.com>
2020-10-21 21:19:56 -0400

    Use requests session, add list_folder method, fix bug in nersc_login.ipynb

--- dev/jupyter/reshpc/nersc_interface.py
+++ dev/jupyter/reshpc/nersc_interface.py
@@ -1,4 +1,5 @@
 """"""
+import fnmatch
 import os
 
 import requests
@@ -12,6 +13,7 @@ class NerscInterface:
         """"""
         self._login_notebook = login_notebook  # for error messages (only)
         self._newt_sessionid = None
+        self._requests_session = requests.Session()
 
 
     def get_job_info(self, job_id, machine=MACHINE, verbose=False):
@@ -20,9 +22,8 @@ class NerscInterface:
         if verbose:
             print('sending command...')
 
-        cookies = dict(newt_sessionid=self._newt_sessionid)
         url = '{}/queue/{}/{}/sacct'.format(NEWT_BASE_URL, machine, job_id)
-        r = requests.get(url, cookies=cookies)
+        r = self._requests_session.get(url)
         r.raise_for_status()
 
         js = r.json()
@@ -38,18 +39,41 @@ class NerscInterface:
         assert self._newt_sessionid is not None
         print('sending command...')
 
-        cookies = dict(newt_sessionid=self._newt_sessionid)
         data = {
           'executable': 'echo $SCRATCH',
           'loginenv': 'true'
         }
         url = '%s/command/%s' % (NEWT_BASE_URL, machine)
-        r = requests.post(url, cookies=cookies, data=data)
+        r = self._requests_session.post(url, data=data)
         r.raise_for_status()
 
         js = r.json()
         scratch_folder = js.get('output')
         return scratch_folder
+
+
+    def list_folder(self, nersc_folder, glob_pattern=None, machine=MACHINE):
+        """Returns a GENERATOR with contents of a folder.
+
+        Args:
+            pattern: string for matching filename, e.g., '*.xml'
+        """
+        url = '%s/file/%s/%s' % (NEWT_BASE_URL, machine, nersc_folder)
+        r = self._requests_session.get(url)
+        r.raise_for_status()
+
+        paths = r.json()
+
+        for path in paths:
+            if glob_pattern is not None and \
+                    not fnmatch.fnmatch(path['name'], glob_pattern):
+                continue
+
+            del path['perms']
+            del path['hardlinks']
+
+            path['size'] = int(path['size'])
+            yield path
 
 
     def login(self, file='~/.newt_sessionid', newt_sessionid=None):
@@ -65,8 +89,8 @@ class NerscInterface:
         assert newt_sessionid is not None
         print('sending command...')
 
-        cookies = dict(newt_sessionid=newt_sessionid)
         url = '{}/login/'.format(NEWT_BASE_URL)
+        cookies = dict(newt_sessionid=newt_sessionid)
         r = requests.get(url, cookies=cookies)
         r.raise_for_status()
 
@@ -75,13 +99,16 @@ class NerscInterface:
         # Example reply:
         # {'username': 'johnt', 'session_lifetime': 976394, 'auth': True, 'newt_sessionid': '0fc3f5310b54310f08bdcbf690d5c255'}
         if 'auth' not in js or not js['auth']:
+            print('Reply:', js)
             raise Exception('User not logged in => use {}'.format(self._login_notebook))
 
         if hasattr(js, 'session_lifetime') and js['session_lifetime'] < 300:
+            print('Reply:', js)
             template = 'Session lifetime about to expire ({} sec) => use {}'
             raise Exception(template.format(js['session_lifetime'], self._login_notebook))
 
         self._newt_sessionid = newt_sessionid
+        self._requests_session.cookies.set('newt_sessionid', self._newt_sessionid)
         return 'OK'
 
 
@@ -89,13 +116,12 @@ class NerscInterface:
         assert self._newt_sessionid is not None
         print('sending command...')
 
-        cookies = dict(newt_sessionid=self._newt_sessionid)
         data = {
           'executable': 'mkdir -p {}'.format(nersc_folder),
           'loginenv': 'true'
         }
         url = '%s/command/%s' % (NEWT_BASE_URL, machine)
-        r = requests.post(url, cookies=cookies, data=data)
+        r = self._requests_session.post(url, data=data)
         r.raise_for_status()
         return 'OK'
 
@@ -111,8 +137,7 @@ class NerscInterface:
 
         print('uploading slurm script...')
         url = '{}/file/{}{}'.format(NEWT_BASE_URL, machine, nersc_folder)
-        cookies = dict(newt_sessionid=self._newt_sessionid)
-        r = requests.post(url, cookies=cookies, files={'file': (job_filename, slurm_script)})
+        r = self._requests_session.post(url, files={'file': (job_filename, slurm_script)})
         r.raise_for_status()
 
         print('submitting job...')
@@ -120,7 +145,7 @@ class NerscInterface:
         data = {
             'jobfile': '{}/{}'.format(nersc_folder, job_filename)
         }
-        r = requests.post(url, cookies=cookies, data=data)
+        r = self._requests_session.post(url, data=data)
         r.raise_for_status()
 
         js = r.json()
@@ -138,7 +163,7 @@ class NerscInterface:
         filename = os.path.basename(local_path)
         with open(local_path, 'rb') as f:
             # Found the syntax for "files" from cumulus.transport.newt:
-            r = requests.post(url, cookies=cookies, files={'file': (filename, f)})
+            r = self._requests_session.post(url, files={'file': (filename, f)})
             r.raise_for_status()
         return 'OK'
 
